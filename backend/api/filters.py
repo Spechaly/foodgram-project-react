c65dc112aa295298841f21d002130d13c20b3caf
spechaly <baluew.1998@mail.ru>
2023-06-20 14:49:47 +0300

    все работает

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,6 +1,7 @@
 from django_filters import rest_framework as filters
 
 from recipes.models import Recipe, Tag, Ingredient
+from django_filters import rest_framework
 
 
 class RecipeFilter(filters.FilterSet):
@@ -11,19 +12,35 @@ class RecipeFilter(filters.FilterSet):
         to_field_name='slug',
         queryset=Tag.objects.all()
     )
-    author = filters.NumberFilter(field_name='author__id')
+    author = rest_framework.NumberFilter(
+        field_name='author',
+        lookup_expr='exact'
+    )
+    is_favorited = filters.BooleanFilter(method='filter_is_favorited')
+    is_in_shopping_cart = filters.BooleanFilter(
+        method='filter_is_in_shopping_cart')
 
     class Meta:
         model = Recipe
         fields = ('tags', 'author')
 
+    def filter_is_favorited(self, queryset, name, value):
+        user = self.request.user
+        if value and not user.is_anonymous:
+            return queryset.filter(favorites__user=user)
+        return queryset
+
+    def filter_is_in_shopping_cart(self, queryset, name, value):
+        user = self.request.user
+        if value and not user.is_anonymous:
+            return queryset.filter(shopping__user=user)
+        return queryset
+
 
 class IngredientFilter(filters.FilterSet):
     """Фильтр ингредиентов."""
 
-    name = filters.CharFilter(
-        lookup_expr='icontains'
-    )
+    name = filters.CharFilter(lookup_expr='startswith')
 
     class Meta:
         model = Ingredient
