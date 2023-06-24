cc11efdb2f03f0ccda1505d98a9b038324839112
spechaly <baluew.1998@mail.ru>
2023-06-24 17:07:03 +0300

    h

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -2,7 +2,7 @@ from django.core import paginator
 from rest_framework.pagination import PageNumberPagination
 
 
-class CustomPagination(PageNumberPagination):
+class PageLimitPagination(PageNumberPagination):
     django_paginator_class = paginator.Paginator
     page_size_query_param = 'limit'
     page_size = 6
