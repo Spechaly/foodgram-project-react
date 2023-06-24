53336d49e625b982db32fea3380d98946146e714
spechaly <baluew.1998@mail.ru>
2023-06-24 17:40:04 +0300

    t

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -106,7 +106,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'api.pagination.CustomPagination',
+    'DEFAULT_PAGINATION_CLASS': 'api.pagination.PageLimitPagination',
 }
 
 DJOSER = {
